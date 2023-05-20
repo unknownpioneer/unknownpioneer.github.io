2417d3de62503e9990755812c6ad83f901a8a6b0
BEUFK <114795041+unknownpioneer@users.noreply.github.com>
2023-05-20 19:05:02 +0800

    Update maintain.js

--- maintain.js
+++ maintain.js
@@ -1,9 +1,4 @@
 var active = 1;
 if (active == 1) {
-    var xhr = new XMLHttpRequest();
-    xhr.open('GET', '/path/to/resource', true);
-    xhr.status = 502;
-    xhr.send();
-
     window.location.href = "nope.html"
 }
